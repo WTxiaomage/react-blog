f40a2781fdd9ca3a915b1fb192f85dc346e44f7a
tao.wang <331871902@qq.com>
2021-12-01 22:56:38 +0800

    feature：前台联通

--- blog/config/apiUrl.js
+++ blog/config/apiUrl.js
@@ -2,10 +2,11 @@
  * @Author: wangtao
  * @Date: 2021-11-17 13:58:29
  * @LastEditors: 汪滔
- * @LastEditTime: 2021-11-25 20:06:30
+ * @LastEditTime: 2021-12-01 22:51:51
  * @Description: file content
  */
-let ipUrl = 'http://127.0.0.1:7001/default/' 
+// let ipUrl = 'http://127.0.0.1:7001/default/' //开发
+let ipUrl = 'http://49.235.127.70:7001/default/' //生产
 
 let servicePath = {
     getArticleList:ipUrl + 'getArticleList' ,  //  首页文章列表接口
