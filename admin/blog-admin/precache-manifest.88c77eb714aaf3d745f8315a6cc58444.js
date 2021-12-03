d6352f887fcc12fb53a8b434542b00126f027464
tao.wang <331871902@qq.com>
2021-12-03 16:57:22 +0800

    feature：test

---
+++ admin/blog-admin/precache-manifest.88c77eb714aaf3d745f8315a6cc58444.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe3ba430236dee6bef02d4ed3cf93774",
+    "url": "./index.html"
+  },
+  {
+    "revision": "42ea2649a06798538f72",
+    "url": "./static/css/2.929389fa.chunk.css"
+  },
+  {
+    "revision": "ddda08d91f513312323e",
+    "url": "./static/css/main.247c6cb3.chunk.css"
+  },
+  {
+    "revision": "42ea2649a06798538f72",
+    "url": "./static/js/2.f53731a6.chunk.js"
+  },
+  {
+    "revision": "ddda08d91f513312323e",
+    "url": "./static/js/main.21cc2495.chunk.js"
+  },
+  {
+    "revision": "56b7bca8931a1f6dbe8e",
+    "url": "./static/js/runtime-main.af2fb581.js"
+  }
+]);
